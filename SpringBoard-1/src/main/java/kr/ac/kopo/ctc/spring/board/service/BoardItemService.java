ac32bd92e3a8723f3a51614ba6cfc0988f0dfe2f
shchoi <101681471+shchoichoko@users.noreply.github.com>
2022-07-14 15:16:18 +0900

    AOP,Transaction,cache

--- SpringBoard-1/src/main/java/kr/ac/kopo/ctc/spring/board/service/BoardItemService.java
+++ SpringBoard-1/src/main/java/kr/ac/kopo/ctc/spring/board/service/BoardItemService.java
@@ -1,8 +1,11 @@
 package kr.ac.kopo.ctc.spring.board.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class BoardItemService {
+public interface BoardItemService {
+	void test();
+	void testAopBefore();
+	void testAopAfter();
+	String testAopAfterReturning();
+	void testAopAfterThrowing();
+	void testAopAround();
 
 }
