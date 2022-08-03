58e67591f890c713b57c03d762d3ae493a52e6c4
shchoi <101681471+shchoichoko@users.noreply.github.com>
2022-08-03 17:27:08 +0900

    자료구조 공부

---
+++ DataStructure/src/list/linkedlist/implement/LinkedList.java
@@ -0,0 +1,7 @@
+package list.linkedlist.implement;
+
+public class LinkedList {
+
+	
+
+}
---
+++ DataStructure/src/list/linkedlist/implement/Main.java
@@ -0,0 +1,10 @@
+package list.linkedlist.implement;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+			LinkedList lk = new LinkedList();
+	}
+
+}
